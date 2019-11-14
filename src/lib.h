edc5d93898f4a6a867efc75e8e3a1479faa85c31
Dmitry Barskov <barskoidim@gmail.com>
2019-11-14 12:04:43 +0300

    update lib

--- src/lib.h
+++ src/lib.h
@@ -1,20 +1,67 @@
 #include <string>
 #include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+using std::string;
+using std::to_string;
 
 const int ARRAY_MAX_VALUE = 128;
 
-int* randomArray(int size) {
-    int* array = new int[size];
-    for (int i; i < size; i++) {
-        array[i] = rand() % ARRAY_MAX_VALUE;
+int *randomArray(int);
+string toString(int *, int);
+void setSeed();
+
+void logGot(int, string);
+void logSent(int, string);
+void log(int, string, string);
+
+int *randomArray(int size)
+{
+    setSeed();
+    int *array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = (rand() % ARRAY_MAX_VALUE) + 1;
     }
     return array;
 }
 
-std::string toString(int* array, int size) {
-    std::string string = "[" + std::to_string(array[0]);
-    for (int i = 1; i < size; i++) {
-        string += ", " + std::to_string(array[i]);
+string toString(int *array, int size)
+{
+    if (size == 0)
+    {
+        return "[]";
+    }
+    string string = "[" + to_string(array[0]);
+    for (int i = 1; i < size; i++)
+    {
+        string += ", " + to_string(array[i]);
     }
     return string + "]";
+}
+
+bool seed_is_set = false;
+void setSeed()
+{
+    if (!seed_is_set)
+    {
+        srand(time(0));
+        seed_is_set = true;
+    }
+}
+
+void log(int rank, string action, string message)
+{
+    std::cout << "Process#" + to_string(rank) + action + message + "\n";
+}
+
+void logGot(int rank, string message)
+{
+    log(rank, " got: ", message);
+}
+
+void logSent(int rank, string message)
+{
+    log(rank, " sent: ", message);
 }
