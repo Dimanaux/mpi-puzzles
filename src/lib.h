e716fc7f4b029f186e50701a95a3ac78a7fe201b
Dmitry Barskov <barskoidim@gmail.com>
2019-12-11 22:26:39 +0300

    wip

--- src/lib.h
+++ src/lib.h
@@ -17,8 +17,7 @@ void logGot(int, string);
 void logSent(int, string);
 void log(int, string, string);
 
-int *randomArray(int size)
-{
+int* randomArray(int size) {
     setSeed();
     int *array = new int[size];
     for (int i = 0; i < size; i++)
@@ -28,52 +27,68 @@ int *randomArray(int size)
     return array;
 }
 
-float *randomFloatArray(int size)
-{
+float** randomMatrix(int rows, int cols) {
     setSeed();
-    float *array = new float[size];
-    for (int i = 0; i < size; i++)
-    {
+    float** matrix = new float*[rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = randomFloatArray(cols);
+    }
+    return matrix;
+}
+
+float** transpose(float** matrix, int rows, int cols) {
+    float** transposed = new float*[cols];
+    for (int i = 0; i < cols; i++) {
+        transposed[i] = new float[rows];
+        for (int j = 0; j < rows; j++) {
+            transposed[i][j] = matrix[j][i];
+        }
+    }
+    return transposed;
+}
+
+float* randomFloatArray(int size) {
+    setSeed();
+    float* array = new float[size];
+    for (int i = 0; i < size; i++) {
         array[i] = ((rand() % ARRAY_MAX_VALUE) + 1.0f) / ARRAY_MAX_VALUE;
     }
     return array;
 }
 
-string toString(int *array, int size)
-{
-    if (size == 0)
-    {
+template<typename T>
+string toString(T* array, int size) {
+    if (size == 0) {
         return "[]";
     }
     string string = "[" + to_string(array[0]);
-    for (int i = 1; i < size; i++)
-    {
+    for (int i = 1; i < size; i++) {
         string += ", " + to_string(array[i]);
     }
     return string + "]";
 }
 
 bool seed_is_set = false;
-void setSeed()
-{
-    if (!seed_is_set)
-    {
+void setSeed() {
+    if (!seed_is_set) {
         srand(time(0));
         seed_is_set = true;
     }
 }
 
-void log(int rank, string action, string message)
-{
+void log(int rank, string action, string message) {
     std::cout << "Process#" + to_string(rank) + action + message + "\n";
 }
 
-void logGot(int rank, string message)
-{
+void logGot(int rank, string message) {
     log(rank, " got: ", message);
 }
 
-void logSent(int rank, string message)
-{
+void logSent(int rank, string message) {
     log(rank, " sent: ", message);
+}
+
+template<typename T>
+T abs(T t) {
+    return t < 0 ? -t : t;
 }
