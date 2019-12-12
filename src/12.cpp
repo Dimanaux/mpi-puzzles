2316afd18e23527c2c521208f2a4de2997e67e45
Dmitry Barskov <barskoidim@gmail.com>
2019-12-12 11:50:46 +0300

    fix errors

--- src/12.cpp
+++ src/12.cpp
@@ -9,36 +9,36 @@ using namespace std;
 const int VECTOR_SIZE = 144;
 
 int main(int argc, char** argv) {
-	int rank, size;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     int* vector1;
     int* vector2;
     int scalar_product;
-	
+
     if (rank == 0) {
-		vector1 = randomArray(VECTOR_SIZE);
-		vector2 = randomArray(VECTOR_SIZE);
-	}
-
-	int chunk_size = VECTOR_SIZE / size;
-	int* chunk1 = new int[chunk_size];
-	int* chunk2 = new int[chunk_size];
-
-	MPI_Scatter(vector1, chunk_size, MPI_INT, chunk1, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Scatter(vector2, chunk_size, MPI_INT, chunk2, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
-    
-	int partial_product = 0;
-	for (int i = 0; i < chunk_size; i++) {
-		partial_product += chunk1[i] * chunk2[i];
-	}
-
-	MPI_Reduce(&partial_product, &scalar_product, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	
-    if (rank == 0) {
-		cout << scalar_product << endl;
+        vector1 = randomArray(VECTOR_SIZE);
+        vector2 = randomArray(VECTOR_SIZE);
     }
-	MPI_Finalize();
+
+    int chunk_size = VECTOR_SIZE / size;
+    int* chunk1 = new int[chunk_size];
+    int* chunk2 = new int[chunk_size];
+
+    MPI_Scatter(vector1, chunk_size, MPI_INT, chunk1, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(vector2, chunk_size, MPI_INT, chunk2, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int partial_product = 0;
+    for (int i = 0; i < chunk_size; i++) {
+        partial_product += chunk1[i] * chunk2[i];
+    }
+
+    MPI_Reduce(&partial_product, &scalar_product, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        cout << scalar_product << endl;
+    }
+    MPI_Finalize();
 }
--- src/13.cpp
+++ src/13.cpp
@@ -9,37 +9,37 @@ using namespace std;
 const int VECTOR_SIZE = 144;
 
 int main(int argc, char** argv) {
-	int rank, size;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     float* vector;
     FloatInt min;
 
     if (rank == 0) {
-		vector = randomFloatArray(VECTOR_SIZE);
+        vector = randomFloatArray(VECTOR_SIZE);
         logGot(rank, toString(vector, VECTOR_SIZE));
-	}
+    }
 
-	int chunk_size = VECTOR_SIZE / size;
-	float* chunk = new float[chunk_size];
+    int chunk_size = VECTOR_SIZE / size;
+    float* chunk = new float[chunk_size];
 
-	MPI_Scatter(vector, chunk_size, MPI_INT, chunk, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
-    
-	FloatInt local_min = {chunk[0], 0};
-	for (int i = 0; i < chunk_size; i++) {
-		if (chunk[i] < local_min.value) {
+    MPI_Scatter(vector, chunk_size, MPI_INT, chunk, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    FloatInt local_min = {chunk[0], 0};
+    for (int i = 0; i < chunk_size; i++) {
+        if (chunk[i] < local_min.value) {
             local_min.index = i;
         }
-	}
+    }
     local_min.value = chunk[local_min.index];
 
-	MPI_Reduce(&local_min.index, &min.index, 1, MPI_FLOAT_INT, MPI_MINLOC, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&local_min.index, &min.index, 1, MPI_FLOAT_INT, MPI_MINLOC, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         min.value = vector[min.index];
-		cout << "min " + to_string(min.value) + " at index " + to_string(min.index) << endl;
+        cout << "min " + to_string(min.value) + " at index " + to_string(min.index) << endl;
     }
-	MPI_Finalize();
+    MPI_Finalize();
 }
